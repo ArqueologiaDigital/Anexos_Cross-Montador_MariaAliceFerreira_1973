00e26cd2dc63cea5177de1d896a40e8bc10ccfc6
Felipe Correa da Silva Sanches <juca@members.fsf.org>
2026-01-13 01:31:23 -0300

    Scans do anexo 22

---
+++ anexo_22/make_pdf.sh
@@ -0,0 +1,14 @@
+width=600
+echo "Generating lowres pages:"
+for f in originais/*.png
+do
+    echo "Converting $f ..."
+    convert $f -resize $width $f.lowres.jpg
+done
+
+echo "Generating the final PDF ..."
+convert originais/capa*.lowres.jpg originais/sinopse*.lowres.jpg originais/indice*.lowres.jpg originais/p*.lowres.jpg PDF/Anexo_22_Maria_Alice.pdf
+
+rm originais/*.lowres.jpg
+echo "done."
+
